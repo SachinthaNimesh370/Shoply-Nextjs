feba49e5542ebf344e88b3e5be717a36695d9771
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2025-08-04 10:48:45 +0530

    Modifie the api end point

--- src/app/pages/Product.jsx
+++ src/app/pages/Product.jsx
@@ -11,7 +11,7 @@ export default function Product() {
 
   useEffect(() => {
     // Fetch products from the API
-    fetch('http://localhost:5175/api/Product/all')
+    fetch('http://localhost:5175/api/Product')
       .then((response) => response.json())
       .then((data) => {
         // Filter products by highest rating and limit to top 5
